fb187fddf2387093df69b678f86d4b6b25f2c08c
Yukai Huang <yukaihuangtw@gmail.com>
2018-02-24 09:56:40 +0800

    Implment basic kkbox api client

---
+++ api-gateway/src/api.ts
@@ -0,0 +1,65 @@
+import { Auth, Api } from '@kkbox/kkbox-js-sdk'
+import Store from './store'
+
+interface IAuthData {
+  access_token: string;
+  expires_in: number;
+}
+
+class API {
+  private auth : Auth
+  private accessToken? : string
+  private store : Store
+  private apiClient : Api
+
+  constructor() {
+    this.auth = new Auth(process.env.KKBOX_APP_ID, process.env.KKBOX_APP_SECRET)
+    this.store = new Store()
+    this.accessToken = undefined
+  }
+
+  async initialize() {
+    await this.getAccessToken()
+    this.apiClient = new Api(this.accessToken)
+  }
+
+  async getAccessToken() {
+    const TOKEN_KEY = 'kkbox_access_token'
+    const EXP_KEY = 'kkbox_token_expires_in'
+
+    if (!this.accessToken) {
+      let token
+
+      try {
+        const expires_in = await this.store.get(EXP_KEY)
+        token = await this.store.get(TOKEN_KEY)
+
+        if (new Date() > new Date(expires_in)) {
+          // token expiration
+          token = undefined
+        }
+      } catch (e) {}
+
+      if (typeof token === 'undefined' || token === null) {
+        const authData = await this.generateAccessToken()
+        token = authData.access_token
+
+        await this.store.set(TOKEN_KEY, authData.access_token)
+        await this.store.set(EXP_KEY, authData.expires_in)
+      }
+
+      this.accessToken = token
+    }
+  }
+
+  async generateAccessToken() : Promise<IAuthData> {
+    const { data: { access_token, expires_in } } = await this.auth.clientCredentialsFlow.fetchAccessToken()
+    return { access_token, expires_in }
+  }
+
+  get client () {
+    return this.apiClient
+  }
+}
+
+export default new API()
